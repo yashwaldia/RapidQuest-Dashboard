7b4fcc3b8d563d028965c5e0c123fe8f0cc20aae
yashwaldia <115491299+yashwaldia@users.noreply.github.com>
2024-01-14 19:48:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import MainSection from "./components/MainSection/MainSection";
 import "./App.css";
-import BNavbar from "./components/MainSection/bnav/BNavbar";
+
 function App() {
   const [sideNavToggle, setSideNavToggle] = useState({
     screenWidth: 0,
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <BNavbar />
+     
         <Navbar onToggleSideNav={handleToggleSideNav} />
         <MainSection />
       </div>
